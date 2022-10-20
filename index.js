095cb9ed42818db34a0a33172200f91737eabe8e
Rhowen01 <rraju002@ucr.edu>
2022-10-20 15:12:02 -0700

    update to the index.js and generateHTML

---
+++ index.js
@@ -0,0 +1,179 @@
+//page creation
+const generateHTML = require('./src/generateHTML');
+
+//profiles
+const Coach = require('./lib/Coach');
+const Engineer = require('./lib/Manager');
+const Player = require('./lib/Player'); 
+
+//node modules
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+
+//team array 
+const teamArray = []
+
+//begin coach prompt 
+const addCoach = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Who coaches UC Riverside?', 
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("Please enter Coach _________ name!");
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'position',
+            message: "Coach is the ____ coach.",
+            validate: nameInput => {
+                if  (isNaN(nameInput)) {
+                    console.log ("Please enter coach's position!")
+                    return false; 
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter the Coach's email.",
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log ('Please enter an email!')
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "Where is Coach's office located?",
+            validate: nameInput => {
+                if  (isNaN(nameInput)) {
+                    console.log ('Please enter his office location!')
+                    return false; 
+                } else {
+                    return true;
+                }
+            }
+        }
+    ])
+    .then(coachInput => {
+        const  { name, id, email, officeNumber } = coachInput; 
+        const coach = new Coach (name, id, email, officeNumber);
+
+        teamArray.push(coach); 
+        console.log(coach); 
+    })
+};
+
+const addManager = () => {
+    console.log(`
+    =================
+    Adding manager to the team
+    =================
+    `);
+
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Who is the Head Manager at UC Riverside?', 
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("Please enter his name!");
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'position',
+            message: "He is the ____ Manager of UC Riverside.",
+            validate: nameInput => {
+                if  (isNaN(nameInput)) {
+                    console.log ("Please enter the manager's position!")
+                    return false; 
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter the manager's email.",
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log ('Please enter an email!')
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'year',
+            message: "Where year is the Head Manager?",
+            validate: nameInput => {
+                if  (isNaN(nameInput)) {
+                    console.log ('Please enter his academic year!')
+                    return false; 
+                } else {
+                    return true;
+                }
+            }
+        }
+    ])
+    .then(managerInput => {
+        const  { name, id, email, year } = managerInput; 
+        const manager = new Manager (name, id, email, year);
+
+        teamArray.push(manager); 
+        console.log(manager); 
+    })
+};
+
+
+// function to generate HTML page file using file system 
+const writeFile = data => {
+    fs.writeFile('./index.html', data, err => {
+        // if there is an error 
+        if (err) {
+            console.log(err);
+            return;
+        // when the profile has been created 
+        } else {
+            console.log("Your team profile has been successfully created! Please check out the index.html")
+        }
+    })
+}; 
+
+addManager()
+  .then(addEmployee)
+  .then(teamArray => {
+    return generateHTML(teamArray);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .catch(err => {
+ console.log(err);
+  });
---
+++ src/generateHTML.js
@@ -0,0 +1,137 @@
+//create Coach card
+const generateCoach = function (Coach) {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${Coach.name}</h3>
+                <h4>Coach</h4><i class="material-icons">content_paste</i>
+            </div>
+            <div class="card-body">
+                <p class="position">Position: ${Coach.position}</p>
+                <p class="email">Email: <a href="mailto:${Coach.email}">${Coach.email}</a></p>
+                <p class="office">Office Number: ${Coach.officeNumber}</p>
+            </div>
+        </div>
+    </div>
+    `;
+
+
+}
+
+//create Manager card
+const generateManager = function (manager) {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${manager.name}</h3>
+                <h4>Manager</h4>
+            </div>
+            <div class="card-body">
+                <p class="position">position: ${manager.position}</p>
+                <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+                <p class="github">Github: <a href="https://github.com/${manager.github}">${manager.github}</a></p>
+            </div>
+        </div>
+    </div>
+    `
+}
+
+//create Player Card
+const generatePlayer = function (Player) {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${Player.name}</h3>
+                <h4>Player</h4><i class="material-icons">assignment_ind</i>
+            </div>
+            <div class="card-body">
+                <p class="position">position: ${Player.position}</p>
+                <p class="height">Height: 6'4 </p>
+                <p class="hometown">Hometown: ${Player.hometown}</p>
+            </div>
+    </div>
+</div>
+    `
+};
+
+//push array to page
+generateHTML = (data) => {
+    //array for cards
+    pageArray = [];
+    for (let i = 0; i < data.length; i++) {
+        const staff = data[i];
+        const role = staff.getRole();
+        
+        //call Coach function
+        if (role === 'Coach') {
+            const coachCard = generateCoach(staff);
+
+            pageArray.push(coachCard);
+        }
+    }
+
+    //call Manager function
+    if (role === 'Manager') {
+        const managerCard = generateManager(staff);
+
+        pageArray.push(managerCard);
+    }
+
+    //call Player function
+    if (role=== 'Player') {
+        const playerCard = generatePlayer(staff);
+        pageArray.push(playerCard)
+    }
+}
+
+//joining strings 
+const staffCards = pageArray.join ('')
+
+//go back to generated page 
+const generateTeamage = function (staffCards) {
+    return generateTeam;
+}
+
+//create HTML page
+const generateTeamPage = function (staffCards) {   
+    return`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <nav class="navbar" id="navbar">
+                <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+            </nav>
+        </header>
+        <main>
+            <div class="container">
+                <div class="row justify-content-center" id="team-cards">
+                    <!--Team Cards-->
+                    ${staffCards}
+                </div>
+            </div>
+        </main>
+        
+    </body>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    </html>
+  `;
+  }
+  
+  // export to index
+  module.exports = generateHTML; 
